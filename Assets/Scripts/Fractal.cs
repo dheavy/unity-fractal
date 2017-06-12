5f5fc59698ded77305bf632fbe097c4fc7a965c5
Davy Peter Braun <davy.braun@callr.com>
2017-06-12 17:58:51 +0200

    Fractal creation of objects using coroutine

--- Assets/Scripts/Fractal.cs
+++ Assets/Scripts/Fractal.cs
@@ -6,6 +6,7 @@ public class Fractal : MonoBehaviour {
     public Mesh mesh;
     public Material material;
     public int maxDepth;
+    public float childScale;
 
     private int depth;
 
@@ -14,18 +15,37 @@ public class Fractal : MonoBehaviour {
         gameObject.AddComponent<MeshFilter>().mesh = mesh;
         gameObject.AddComponent<MeshRenderer>().material = material;
         if (depth < maxDepth) {
-            new GameObject("Fractal Child")
-                .AddComponent<Fractal>()
-                .Initialize(this);
+            StartCoroutine(CreateChildren());
         }
     }
 
-    private void Initialize (Fractal parent) 
+    private IEnumerator CreateChildren () 
+    {
+        yield return new WaitForSeconds(0.5f);
+		new GameObject("Fractal Child")
+				.AddComponent<Fractal>()
+				.Initialize(this, Vector3.up);
+
+		yield return new WaitForSeconds(0.5f);
+		new GameObject("Fractal Child")
+				.AddComponent<Fractal>()
+				.Initialize(this, Vector3.left);
+
+        yield return new WaitForSeconds(0.5f);
+		new GameObject("Fractal Child")
+				.AddComponent<Fractal>()
+				.Initialize(this, Vector3.right);
+    }
+
+    private void Initialize (Fractal parent, Vector3 direction) 
     {
         mesh = parent.mesh;
         material = parent.material;
         maxDepth = parent.maxDepth;
         depth = parent.depth + 1;
+        childScale = parent.childScale;
         transform.parent = parent.transform;
+        transform.localScale = Vector3.one * childScale;
+        transform.localPosition = direction * (0.5f + 0.5f * childScale);
     }
 }
