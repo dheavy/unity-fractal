24ae6c19ee20df225ef537877cddd2a6020a9156
Davy Peter Braun <davy.braun@callr.com>
2017-06-12 19:26:26 +0200

    Refactor children creation and add more branches to fractals

--- Assets/Scripts/Fractal.cs
+++ Assets/Scripts/Fractal.cs
@@ -10,6 +10,22 @@ public class Fractal : MonoBehaviour {
 
     private int depth;
 
+    private static Vector3[] childDirections = {
+        Vector3.up,
+        Vector3.right,
+        Vector3.left,
+        Vector3.forward,
+        Vector3.back
+	};
+
+    private static Quaternion[] childOrientations = {
+        Quaternion.identity,
+        Quaternion.Euler(0f, 0f, -90f),
+        Quaternion.Euler(0f, 0f, 90f),
+        Quaternion.Euler(90f, 0f, 0f),
+        Quaternion.Euler(-90f, 0f, 0f)
+    };
+
     private void Start()
     {
         gameObject.AddComponent<MeshFilter>().mesh = mesh;
@@ -21,20 +37,13 @@ public class Fractal : MonoBehaviour {
 
     private IEnumerator CreateChildren () 
     {
-        yield return new WaitForSeconds(0.5f);
-		new GameObject("Fractal Child")
-				.AddComponent<Fractal>().Initialize(this, Vector3.up, Quaternion.identity);
-
-		yield return new WaitForSeconds(0.5f);
-		new GameObject("Fractal Child")
-				.AddComponent<Fractal>().Initialize(this, Vector3.left, Quaternion.Euler(0f, 0f, -90f));
-
-        yield return new WaitForSeconds(0.5f);
-		new GameObject("Fractal Child")
-				.AddComponent<Fractal>().Initialize(this, Vector3.right, Quaternion.Euler(0f, 0f, 90f));
+        for (int i = 0; i < childDirections.Length; i++) {
+            yield return new WaitForSeconds(0.5f);
+            new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, i);
+        }
     }
 
-    private void Initialize (Fractal parent, Vector3 direction, Quaternion orientation)
+    private void Initialize (Fractal parent, int childIndex)
     {
         mesh = parent.mesh;
         material = parent.material;
@@ -43,7 +52,7 @@ public class Fractal : MonoBehaviour {
         childScale = parent.childScale;
         transform.parent = parent.transform;
         transform.localScale = Vector3.one * childScale;
-        transform.localPosition = direction * (0.5f + 0.5f * childScale);
-        transform.localRotation = orientation;
+        transform.localPosition = childDirections[childIndex] * (0.5f + 0.5f * childScale);
+        transform.localRotation = childOrientations[childIndex];
     }
 }
